a103c9b8e01e4e1627e23ac307754fc26caac050
Renzo Crisóstomo <ruenzuo@gmail.com>
2017-04-15 20:31:06 +0200

    Remove image quantization to save memory

--- lib/lltv/command/generate.rb
+++ lib/lltv/command/generate.rb
@@ -35,21 +35,17 @@ module LLTV
         total_frames = length.to_f * fps.to_f
         delay = length.to_f / total_frames.to_f
         last_image = nil
-        last_image_q = nil
         last_image_histogram = {}
         Dir['*'].reject { |file| file.start_with?('.') }.sort.each_cons(2) do |file_name_1, file_name_2|
           image_1 = last_image
           image_1 ||= generate_image(file_name_1, delay)
           image_2 = generate_image(file_name_2, delay)
-          q_1 = last_image_q
-          q_1 ||= image_1.quantize(Default.number_of_colors, Magick::RGBColorspace)
-          q_2 = image_2.quantize(Default.number_of_colors, Magick::RGBColorspace)
           colors = ['red', 'green', 'blue']
           result = 0
           colors.each do |color|
             histogram_1 = last_image_histogram[color]
-            histogram_1 ||= q_1.specific_color_histogram(color)
-            histogram_2 = q_2.specific_color_histogram(color)
+            histogram_1 ||= image_1.specific_color_histogram(color)
+            histogram_2 = image_2.specific_color_histogram(color)
             Default.histogram_bucket_size.times do |time|
               result += (histogram_1[time] - histogram_2[time]).abs
             end
@@ -58,7 +54,6 @@ module LLTV
           Output.out("Comparison result between #{file_name_1} and #{file_name_2}: #{result}")
           image_1.delay = delay
           last_image = image_2
-          last_image_q = q_2
         end
         delay_param = "%.2f" % delay
         `convert -delay #{delay} -loop 0 screenshot_*.jpeg love_live.gif`
